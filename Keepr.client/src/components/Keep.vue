23c82777abcc4e8c008bcb88c39f8745bbcc9729
Talan Weeks <Talan.jakoby.weeks@gmail.com>
2021-11-02 12:17:43 -0600

    account fixed

--- Keepr.client/src/components/Keep.vue
+++ Keepr.client/src/components/Keep.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="d-flex bg-black">    
     <div class="card m-4 shadow rounded " style="width: 18rem">
-      <div class="on-hover position-absolute" style="right: 1rem; top: 1rem" v-if="account.id == keep.creatorId">
-        <i class="mdi mdi-delete-forever text-danger f-20 action" title="delete" @click="deleteKeep()"></i>
-      </div>
       <img :src="keep.img" 
           class="card-img"
           />
-      <div class="card-img-overlay text-light">
+      <div class="card-img-overlay text-light" v-if="keep.creator">
         <span class="position-absolute bottom-0 end-0 me-2 action"  title="details" data-bs-toggle="modal"
           :data-bs-target="'#keep-modal-' + keep.id"><h5>{{keep.name}}</h5></span>
         <router-link :to="{name: 'Profile', params: {id: keep.creatorId}}" class="action position-absolute bottom-0 start-0 m-2" title="profile page">
           <img :src="keep.creator.picture" class="rounded-circle" style="width: 2.5rem;">
-        </router-link>        
+        </router-link> 
+        <div class="on-hover position-absolute" style="right: 1rem; top: 1rem" v-if="account.id == keep.creatorId">
+          <i class="mdi mdi-delete-forever text-danger f-20 action" title="delete" @click="deleteKeep()"></i>
+        </div>       
     </div>      
     </div>
   </div>
--- Keepr/Models/Account.cs
+++ Keepr/Models/Account.cs
@@ -2,8 +2,6 @@ namespace Keepr.Models
 {
   public class Account : Profile
     {
-        public new string Id { get; set; }
-
         public string Email { get; set; }
 
     }
