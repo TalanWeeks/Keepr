3f52422d8e385f8bf787be970ff53319d55230f9
Talan Weeks <Talan.jakoby.weeks@gmail.com>
2021-11-22 14:48:28 -0700

    dbs setup

--- Keepr/dbSetup.sql
+++ Keepr/dbSetup.sql
@@ -8,15 +8,36 @@ CREATE TABLE IF NOT EXISTS accounts(
 ) default charset utf8 COMMENT '';
 
 
-      SELECT
-      vk.id AS vaultKeepId,
-      k.*,
-      a.*
-      FROM vault_keeps vk
-      JOIN keeps k ON k.id = vk.keepId
-      JOIN accounts a ON a.id = vk.creatorId
-      WHERE vk.vaultId = 622;
+CREATE TABLE IF NOT EXISTS keeps (
+   id INT NOT NULL AUTO_INCREMENT primary key COMMENT 'primary key',
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  creatorId VARCHAR(255) NOT NULL COMMENT 'Foreign Key',
+  name varchar(255) COMMENT 'Keep Name',
+  description varchar(255) COMMENT 'Keep Description',
+  img varchar(255) COMMENT 'Keep Picture',
+  views int COMMENT 'Keep Views',
+  shares int COMMENT 'Keep Shares',
+  keeps int COMMENT 'Keep Keeps',
+  FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
 
+CREATE TABLE IF NOT EXISTS vaults (
+  id INT NOT NULL AUTO_INCREMENT primary key COMMENT 'primary key',
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  creatorId VARCHAR(255) NOT NULL COMMENT 'Foreign Key',
+  name varchar(255) COMMENT 'Vault Name',
+  description varchar(255) COMMENT 'Vault Description',
+  isPrivate TINYINT DEFAULT 0 COMMENT 'Vault isPrivate',
+  FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
 
-      UPDATE keeps
-      SET views = 0
+CREATE TABLE IF NOT EXISTS vault_keeps (
+  id INT NOT NULL AUTO_INCREMENT primary key COMMENT 'primary key',
+  creatorId VARCHAR(255) NOT NULL comment 'creator ID',
+  keepId INT NOT NULL COMMENT 'Foreign Key',
+  vaultId INT NOT NULL COMMENT 'Foreign Key',
+  FOREIGN KEY (keepId) REFERENCES keeps(id) ON DELETE CASCADE,
+  FOREIGN KEY (vaultId) REFERENCES vaults(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
