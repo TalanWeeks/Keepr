ca89e31f30e27403057f9f0fc2941584f21b4e62
Talan Weeks <Talan.jakoby.weeks@gmail.com>
2021-11-30 13:22:22 -0700

    documentation

--- Keepr.client/src/services/ProfilesService.js
+++ Keepr.client/src/services/ProfilesService.js
@@ -4,6 +4,8 @@ import { logger } from '../utils/Logger'
 import { api } from './AxiosService'
 
 class ProfilesService {
+
+  // a node get method that gets an individuals profile by their ID
   async getProfileById(id) {
     const res = await api.get(`api/profiles/${id}`)
     logger.log('profile res', res)
--- Keepr.client/src/services/VaultsService.js
+++ Keepr.client/src/services/VaultsService.js
@@ -7,18 +7,22 @@ import { logger } from "../utils/Logger"
 import { api } from "./AxiosService"
 class VaultsService{
 
+  // a node post method that allows a user to create a vault and will display it first in order
   async createVault(newVault){
     const res = await api.post('api/vaults', newVault)
     AppState.usersVaults.unshift(new Vault(res.data))
     logger.log('your new vault data sir', res.data)
   }
 
+  // a node post method that allows a user to create a vaultKeep and will display it first in order
   async createVaultKeep(vaultId, keepId){
     const data = {vaultId: vaultId, keepId: keepId}
     const res = await api.post('api/vaultkeeps', data)
     AppState.usersVaultKeeps.unshift(new VaultKeep(res.data))
     logger.log('your new vault data sir', res.data)
   }
+
+  // a node get method that allows a user to get all vaults by a specific profile ID
   async getVaultsByProfileId(profileId){
     AppState.usersVaults = []
     const res = await api.get(`api/profiles/${profileId}/vaults`)
@@ -26,12 +30,15 @@ class VaultsService{
     AppState.usersVaults = res.data.map(v => new Vault(v))
   }
 
+  // a node get method that allows a user to get one vault by its ID and is used to open/ view the vaults details
   async getVaultById(vaultId){
     AppState.vault = {}
     const res = await api.get(`api/vaults/${vaultId}`)
     logger.log("here is the vault tied to this page", res.data)
     AppState.vault = res.data
   }
+
+  // a put method that allows a user to edit a vault typically used to add a vaultKeep into it
   async editVault(vaultData){
     const res = await api.put(`api/vaults/${vaultData.id}`, vaultData)
     logger.log('your edited vault data mi lord', res.data)
@@ -39,6 +46,7 @@ class VaultsService{
     AppState.usersVaults.splice(vaultIndex, 1, new Vault(res.data))
   }
 
+  //  a delete method that is used to allows a user to delete a vault by its ID
   async delete(id) {
     const res = await api.delete(`api/vaults/${id}`)
     logger.log('delete res', res)
