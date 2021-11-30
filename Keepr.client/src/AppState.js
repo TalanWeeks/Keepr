e67ceb88004519963882f20452ebad05038582c5
Talan Weeks <Talan.jakoby.weeks@gmail.com>
2021-11-30 13:23:22 -0700

    doc

--- Keepr.client/src/AppState.js
+++ Keepr.client/src/AppState.js
@@ -1,6 +1,6 @@
 import { reactive } from 'vue'
 
-// NOTE AppState is a reactive object to contain app level data
+// NOTE AppState is a reactive object to contain app level data this data can be used throughout the app as long as you use a constructor to access the data in that specific component/page
 export const AppState = reactive({
   user: {},
   account: {},
